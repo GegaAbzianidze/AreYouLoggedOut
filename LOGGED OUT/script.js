e66baa77ffe7fc4e59634b63ceb2b82d9e0a8182
Ozzzx <miss.zaqareishvili@gmail.com>
2022-03-25 20:33:28 +0400

    1.0 alpha

---
+++ LOGGED OUT/script.js
@@ -0,0 +1,17 @@
+//create function that find word by class and replace it with new word
+function replaceWord(className, newWord) {
+    var elements = document.getElementsByClassName("d2edcug0");
+    for (var i = 0; i < elements.length; i++) {
+      loadFile();
+      function loadFile() { 
+              
+        window.onbeforeunload = function(){
+        return 'Are you sure you want to leave?';
+      };
+    }
+  }
+}
+replaceWord();
+
+
+
