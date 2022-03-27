aafff098a8920d70e03a39d6e33b3cb54a4d9a4c
Ozzzx <miss.zaqareishvili@gmail.com>
2022-03-27 13:49:46 +0400

    add features 1.0.1
    learn communicate to content script and will use next commit

--- LOGGED OUT/popup.js
+++
@@ -1,9 +0,0 @@
-
-function save() {	
-	var checkbox = document.getElementById("checkbox1");
-    localStorage.setItem("checkbox1", checkbox.checked);	
-}
-
-//for loading
-var checked = JSON.parse(localStorage.getItem("checkbox1"));
-    document.getElementById("checkbox1").checked = checked;
